ddd3f45ed3c4a9e246ec46677c7212206cfbc433
Carlos Alexandre Ernst <62969365+CarlosErnst@users.noreply.github.com>
2021-05-19 00:59:10 +0000

    primeira aula

---
+++ carlosernstdasc2021/src/main/java/br/univille/carlosernstdasc2021/Carlosernstdasc2021Application.java
@@ -0,0 +1,13 @@
+package br.univille.carlosernstdasc2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Carlosernstdasc2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Carlosernstdasc2021Application.class, args);
+	}
+
+}
