c0087b9eecad105c09f263240e269209eafdd95e
Carlos Alexandre Ernst <62969365+CarlosErnst@users.noreply.github.com>
2021-06-21 23:36:03 +0000

    Listar Produtos

---
+++ carlosernstdacs2021/src/main/java/br/univille/carlosernstdacs2021/service/ProdutoService.java
@@ -0,0 +1,8 @@
+package br.univille.carlosernstdacs2021.service;
+
+import java.util.List;
+import br.univille.carlosernstdacs2021.model.Produto;
+
+public interface ProdutoService {
+    public List<Produto> getAllProdutos();
+}
